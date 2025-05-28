719ed93a585365bb946f39c750e85fe9dc22970f
Ayush Dattagupta <ayushdg95@gmail.com>
2025-05-27 21:17:24 -0700

    Dump high level skeleton
    Signed-off-by: Ayush Dattagupta <ayushdg95@gmail.com>

---
+++ ray_curator/data/__init__.py
@@ -0,0 +1,5 @@
+"""Data structures for the ray-curator pipeline framework."""
+
+from .task import DocumentBatch, ImageBatch, ImageObject, Task
+
+__all__ = ["DocumentBatch", "ImageBatch", "ImageObject", "Task"]
