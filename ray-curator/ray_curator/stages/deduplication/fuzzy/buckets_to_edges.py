9b22c79ba308cc8bceb9d9c3455b25e7f5eeee16
Ayush Dattagupta <ayushdg95@gmail.com>
2025-08-14 17:30:16 +0000

    Initial buckets to edges stage
    Signed-off-by: Ayush Dattagupta <ayushdg95@gmail.com>

---
+++ ray-curator/ray_curator/stages/deduplication/fuzzy/buckets_to_edges.py
@@ -0,0 +1,64 @@
+from itertools import pairwise
+from typing import Any
+
+import pandas as pd
+import pyarrow as pa
+import pyarrow.parquet as pq
+
+from ray_curator.stages.base import ProcessingStage
+from ray_curator.stages.deduplication.id_generator import CURATOR_DEDUP_ID_STR
+from ray_curator.stages.resources import Resources
+from ray_curator.tasks import FileGroupTask
+from ray_curator.utils.file_utils import get_fs
+
+
+class BucketsToEdgesStage(ProcessingStage[FileGroupTask, FileGroupTask]):
+    """
+    Stage that takes in a file consiting of LSH bucket ids and document ids belonging to the bucket
+    and outputs a file consisting of edges between documents with the same bucket id.
+
+    Args:
+        doc_id_column: The column name containing the document ids for each bucket.
+        output_dir: The directory to write the output file to.
+        read_kwargs: Keyword arguments to pass to the read_table function.
+        write_kwargs: Keyword arguments to pass to the write_table function.
+    """
+
+    _name = "BucketsToEdgesStage"
+    _resources = Resources(cpus=1.0)
+
+    def __init__(
+        self,
+        output_dir: str,
+        doc_id_column: str = CURATOR_DEDUP_ID_STR,
+        read_storage_options: dict[str, Any] | None = None,
+        write_storage_options: dict[str, Any] | None = None,
+    ):
+        self.doc_id_column = doc_id_column
+        self.output_dir = output_dir
+        self.read_storage_options = read_storage_options if read_storage_options is not None else {}
+        self.write_storage_options = write_storage_options if write_storage_options is not None else {}
+
+    def process(self, task: FileGroupTask) -> FileGroupTask:
+        input_fs = get_fs(task.data[0], self.read_storage_options)
+        df = pq.read_table(task.data, filesystem=input_fs)
+        edges = []
+        for bucket_docs in df[self.doc_id_column]:
+            edges.extend(pairwise(bucket_docs))
+        edges = [list(edge) for edge in edges]
+        edges = pa.Table.from_pandas(
+            pd.DataFrame(edges, columns=[f"{self.doc_id_column}_x", f"{self.doc_id_column}_y"])
+        )
+
+        output_fs = get_fs(self.output_dir, self.write_storage_options)
+        output_dir = output_fs.sep.join([self.output_dir, self.name])
+        output_fs.mkdirs(output_dir, exist_ok=True)
+        output_path = output_fs.sep.join([output_dir, f"{task.task_id}.parquet"])
+        pq.write_table(edges, output_path, filesystem=output_fs)
+        return FileGroupTask(
+            task_id=f"{task.task_id}",
+            dataset_name=f"{task.dataset_name}_edges",
+            data=[output_path],
+            _metadata={**task._metadata, "storage_options": self.write_storage_options},
+            _stage_perf=task._stage_perf,
+        )
